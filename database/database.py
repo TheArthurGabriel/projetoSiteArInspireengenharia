1751751a9d06cce444ed18f29e17a46abc8812bb
Arthur Rodrigues <86888848+TheArthurGabriel@users.noreply.github.com>
2024-03-12 12:49:11 -0300

    Teste db

--- database/database.py
+++ database/database.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-db = PostgresqlDatabase('postgresql://ajxnzfay:hcCy9Gm6VkTGkL5-F3O65WW_4UQ159ie@kesavan.db.elephantsql.com/ajxnzfay')
+db = PostgresqlDatabase(os.getenv('DATABASE_URI', ''))
