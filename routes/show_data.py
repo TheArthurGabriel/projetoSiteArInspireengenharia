4011d4eefb70635385b6cec41edba87a5d10d387
Arthur Rodrigues <86888848+TheArthurGabriel@users.noreply.github.com>
2024-03-14 12:41:14 -0300

    Sistemas de ADM, delete/edit

--- routes/show_data.py
+++ routes/show_data.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
 from database.models.laudo import Laudo
+from datetime import datetime
 
 data_route = Blueprint("data", __name__)
 
@@ -7,12 +8,17 @@ data_route = Blueprint("data", __name__)
 def show_data():
     numero_chamado = request.form['numero']
     
-    laudos = Laudo.select().where(Laudo.codigo == numero_chamado)
+    laudos = Laudo.select().where(Laudo.codigo == numero_chamado).order_by(Laudo.date.desc())
+    laudos_ordenados = sorted(laudos, key=lambda laudo: laudo.date, reverse=True)
+
     if len(laudos) > 0:
-        return render_template('laudo.html', data=laudos)
+        return render_template('laudo.html', data=laudos_ordenados)
     else:
         return render_template('homepage.html', error=True)
 
         
-    
-    
+@data_route.route('/<int:id>/delete', methods=['DELETE'])
+def delete(id):
+    target = Laudo.get_by_id(id)
+    target.delete_instance()
+    return 'ok'
